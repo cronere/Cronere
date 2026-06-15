6c8ff52000573767159c39622cae4b0daa752cf7
cronere <merkley.jacob5@gmail.com>
2026-06-15 14:46:47 -0700

    Update page.js

--- app/confirmed/page.js
+++ app/confirmed/page.js
@@ -1,6 +1,6 @@
 'use client'
 
-const LINKEDIN = 'https://www.linkedin.com/in/jacobmerkley/'
+const LINKEDIN = 'https://www.linkedin.com/in/jacob-merkley/'
 const CALENDLY = 'https://calendly.com/jacob-merkley'
 
 const LogoMark = ({ size = 28 }) => (
